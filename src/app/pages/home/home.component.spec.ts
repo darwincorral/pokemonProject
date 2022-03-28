e0c46294180b01439da5e6ab2229c106724e2643
Darwin C. Narváez <35180908+darwincorral@users.noreply.github.com>
2022-03-28 11:51:06 -0500

    Pruebas Unitarias

--- src/app/pages/home/home.component.spec.ts
+++ src/app/pages/home/home.component.spec.ts
@@ -1,5 +1,7 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { HomeComponent } from './home.component';
 
 describe('HomeComponent', () => {
@@ -8,7 +10,9 @@ describe('HomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [ HomeComponent ],
+      imports: [ReactiveFormsModule,HttpClientTestingModule,FormsModule],
+      schemas: [NO_ERRORS_SCHEMA]
     })
     .compileComponents();
   });
@@ -22,4 +26,5 @@ describe('HomeComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
 });
--- src/app/services/pokemon.service.spec.ts
+++ src/app/services/pokemon.service.spec.ts
@@ -1,16 +1,36 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { PokemonService } from './pokemon.service';
 
-describe('PokemonService', () => {
-  let service: PokemonService;
+describe('myService', () => {
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(PokemonService);
-  });
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [HttpClientTestingModule], 
+    providers: [PokemonService]
+  }));
 
-  it('should be created', () => {
+   it('should be created', () => {
+    const service: PokemonService = TestBed.get(PokemonService);
     expect(service).toBeTruthy();
-  });
+   });
+
+   it('should have mostrarPokemons function', () => {
+    const service: PokemonService = TestBed.get(PokemonService);
+    expect(service.mostrarPokemons).toBeTruthy();
+   });
+
+   it('should have crearPokemon function', () => {
+    const service: PokemonService = TestBed.get(PokemonService);
+    expect(service.crearPokemon).toBeTruthy();
+   });
+
+   it('should have editarPokemon function', () => {
+    const service: PokemonService = TestBed.get(PokemonService);
+    expect(service.editarPokemon).toBeTruthy();
+   });
+
+   it('should have eliminarPokemon function', () => {
+    const service: PokemonService = TestBed.get(PokemonService);
+    expect(service.eliminarPokemon).toBeTruthy();
+   });
 });
